3bd9d9cef98dda10e23b122b9b5463501066f6ed
Mathieu JOLY <m.joly@live.fr>
2021-03-08 17:33:47 +0100

    add style layout

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -4,7 +4,12 @@ const SignIn = () => {
 
   return (
     <div className="SignIn">
-        <p>Hello from SignIn!</p>
+        <h1 className="SignIn__title">Se connecter</h1>
+        <form className="SignIn__form">
+        	<input className="SignIn__form__email" type="email" name="email" placeholder="email" />
+        	<input className="SignIn__form__password" type="password" name="password" placeholder="password" />
+        	<input className="SignIn__form__submit" type="submit" />
+        </form>
     </div>
   );
 };
