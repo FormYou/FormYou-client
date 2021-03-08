aad15476dc9a407c29fb209d598b0c29854db420
Mathieu JOLY <m.joly@live.fr>
2021-03-08 15:19:05 +0100

    add Navbar style & layout

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from 'components/Navbar/Navbar';
+import './index.scss';
 
 const Index = () => (
-  <div>
+  <Router>
+  <Navbar />
+  <main>
     <h1>Hello !</h1>
-  </div>
+  </main>
+  </Router>
 );
 
 ReactDOM.render(<Index />, document.getElementById('root'));
