2a607a640763f5ff77709bb59bcf49137a7dc2d0
Gaetan <altgaetan@hotmail.com>
2021-03-09 17:41:53 +0100

    update Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,10 @@ const Hero = () => {
 
   return (
     <div className="Hero">
-        <h1 className="Hero__title">Hello from Hero!</h1>
+        <div className="Hero__presentation">
+          <h1 className="Hero__presentation__title">Pourquoi prendre des cours avec YouForm ? </h1>
+          <h2 className="Hero__presentation__texte">Nous offrons aux entreprises une solution de Digital Learning axée sur l’engagement des utilisateurs, qui inclut une plateforme réunissant une multitude de programmes, et des professeurs dédiés. Nos solutions constament remises à jour sont idéales pour répondre à tous les enjeux de la formation en ligne de vos collaborateurs à grande échelle.</h2>
+        </div>
         <img className="Hero__image" src={courses} alt='laptop online courses'/>
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,17 +5,7 @@ const Home = () => {
 
   return (
     <div className="Home">
-        <Hero />
-        <div className="Home__presentation">
-        	<p>Vos collaborateurs méritent la meilleure Learning Experience Platform</p>
-        	<p>YouForm offre aux entreprises une solution de Digital Learning axée sur l’engagement des utilisateurs, qui inclut une plateforme et des professeurs dédiés pour répondre à tous les enjeux de formation en ligne à grande échelle.</p>
-        	<p>Nous accompagnons vos employés dans la formation continue en ligne</p>
-        	<p>FormYou est une entreprise de vente de formations. Différentes entreprises achètent un forfait chez FormYou. Ainsi, leurs employés peuvent s'inscrire à toutes les formations qu'ils souhaitent, sans limite. Le site de FormYou sert à gérer l'ensemble des utilisateurs. 3 catégories d'utilisateurs existent : Admin, Professeur et Étudiant.
-
-Les formations durent une journée. Il existe plusieurs sessions pour chaque formation. Le nombre d'élèves maximum par session est de 20.
-
-Un professeur est assigné à une formation. Il devra donner cours pour toutes les sessions de cette formation.</p>
-        </div>
+        <Hero />   
     </div>
   );
 };
