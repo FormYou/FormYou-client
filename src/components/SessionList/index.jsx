4e3cc3d3963ba831596542b7db9b16c59e6c25e5
Mathieu JOLY <m.joly@live.fr>
2021-03-11 18:37:13 +0100

    add past & future sessions

--- src/components/SessionList/index.jsx
+++ src/components/SessionList/index.jsx
@@ -5,10 +5,17 @@ import { Link } from 'react-router-dom';
 
 const SessionList = ({ formation_id }) => {
 	const [allSessions, setAllSessions] = useState();
+  const [pastSessions, setPastSessions] = useState();
+  const [futureSessions, setFutureSessions] = useState();
 
   useEffect(() => {
       getSessions()
   }, [])
+
+  useEffect(() => {
+    allSessions && getPast()
+    allSessions && getFuture()
+  }, [allSessions])
 
 	const getSessions = () => {
     fetch(`${api}/formations/${formation_id}/sessions`, {
@@ -21,11 +28,17 @@ const SessionList = ({ formation_id }) => {
     .catch((error) => console.log(error));
 	}
 
-  const formatDate = (dateString) => {
-    return dateString.match
-  } 
+  const getPast = () => {
+    setPastSessions(allSessions.filter(answer => new Date(answer.date).getTime() - Date.now() < 0))
+  }
+
+  const getFuture = () => {
+    setFutureSessions(allSessions.filter(answer => new Date(answer.date).getTime() - Date.now() > 0))
+  }
 
   return (
+    <div>
+    <p>all sessions:</p>
     <ul className="SessionList">
       {allSessions && allSessions.map((session) => (
         <Link to="/">
@@ -33,6 +46,23 @@ const SessionList = ({ formation_id }) => {
         </Link>
       ))}
     </ul>
+    <p>past sessions:</p>
+    <ul className="SessionList">
+      {pastSessions && pastSessions.map((session) => (
+        <Link to="/">
+          <li className="SessionList__item">{`${session.date.substring(8, 10)} ${["janvier","février","mars","avril","mai","juin","juillet","août","septembre","octobre","novembre","décembre",][parseInt(session.date.substring(5, 7) - 1, 10)]} ${session.date.substring(0, 4)}`}</li>
+        </Link>
+      ))}
+    </ul>
+    <p>future sessions:</p>
+    <ul className="SessionList">
+      {futureSessions && futureSessions.map((session) => (
+        <Link to="/">
+          <li className="SessionList__item">{`${session.date.substring(8, 10)} ${["janvier","février","mars","avril","mai","juin","juillet","août","septembre","octobre","novembre","décembre",][parseInt(session.date.substring(5, 7) - 1, 10)]} ${session.date.substring(0, 4)}`}</li>
+        </Link>
+      ))}
+    </ul>
+    </div>
   );
 };
 
